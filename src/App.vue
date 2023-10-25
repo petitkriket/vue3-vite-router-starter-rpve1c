52cba991f615c441c0d901f87c80311bf7224cf8
petitkriket <bouaroua.samuel@gmail.com>
2023-10-25 11:16:46 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { useCssModule } from 'vue'
+const $style = useCssModule()
+console.log($style, 'coucou')
+</script>
+
+<template>
+  <nav>
+    <router-link to="/" :class="$style.toto">Home</router-link>
+    | <router-link to="/about">About</router-link> | <router-link to="/contact">Contact</router-link>
+  </nav>
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style lang="scss" module>
+.toto {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.tata {
+  margin-top: 22px;
+}
+</style>
